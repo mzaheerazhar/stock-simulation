f15888775e3b9e1bc6c422f86cc7832dad6be9bc
Muhammad Zaheer <79625103+mzaheerazhar@users.noreply.github.com>
2023-09-12 19:43:20 +0500

    Fix Commnets

--- market_simulation/common/custom_exceptions.py
+++ market_simulation/common/custom_exceptions.py
@@ -5,26 +5,36 @@ from rest_framework import status
 from rest_framework.exceptions import APIException
 
 import market_simulation.common.constants as constant
+from rest_framework import status
 
 
 class BadRequest(APIException):
     """
     Custom BadRequest exception.
     """
-    status_code = 400
+    status_code = status.HTTP_400_BAD_REQUEST
     default_detail = constant.INVALID_CONTENT
 
 
 class NoContentException(APIException):
+    """
+    Custom No Content exception.
+    """
     status_code = status.HTTP_204_NO_CONTENT
     default_detail = constant.NO_CONTENT
 
 
 class UnProcessableEntity(APIException):
+    """
+    Custom Unprocessable exception.
+    """
     status_code = status.HTTP_422_UNPROCESSABLE_ENTITY
     default_detail = constant.UNPROCESSABLE
 
 
 class NotOwner(APIException):
+    """
+    Custom Unauthrize exception.
+    """
     status_code = status.HTTP_401_UNAUTHORIZED
     default_detail = constant.NOT_OWNER
--- market_simulation/common/helper_methods.py
+++ market_simulation/common/helper_methods.py
@@ -1,5 +1,4 @@
-
 def calculate_price(stock_data, trans):
-    if trans.get('transaction_type') == '0':
+    if trans.get('transaction_type') == 'Buy':
         return stock_data.low * int(trans.get('transaction_volume'))
     return stock_data.high * int(trans.get('transaction_volume'))
